e3aac7e515256d04ff293e77196c86915160b013
isomo <jiahaoxiang2000@gmail.com>
2025-05-07 22:57:29 +0800

    fix: correct __fish_initialized variable value in fish_variables

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    # Set environment variables for IBus to work properly with Electron apps like VSCode
+    set -x GTK_IM_MODULE ibus
+    set -x QT_IM_MODULE ibus
+    set -x XMODIFIERS @im=ibus
 end
+
