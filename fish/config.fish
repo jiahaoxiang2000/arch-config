fdd6be466e787c398cc258d0898c4611403ebf6f
isomo <jiahaoxiang2000@gmail.com>
2025-07-31 22:54:58 +0800

    🔧 chore: comment out proxy settings and SSH tunnel startup in config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -35,9 +35,9 @@ set -x FNM_NODE_DIST_MIRROR https://npmmirror.com/mirrors/node
 set -x NPM_CONFIG_REGISTRY https://registry.npmmirror.com
 
 # proxy settings
-set -x HTTPS_PROXY "http://127.0.0.1:1080"
-set -x HTTP_PROXY "http://127.0.0.1:1080"
-set -x NO_PROXY "localhost,127.0.0.1,::1"
+# set -x HTTPS_PROXY "http://127.0.0.1:1080"
+# set -x HTTP_PROXY "http://127.0.0.1:1080"
+# set -x NO_PROXY "localhost,127.0.0.1,::1"
 
 # source the ~/.profile file, some key is on the ~/.profile
 # source ~/.profile
@@ -45,10 +45,13 @@ set -x NO_PROXY "localhost,127.0.0.1,::1"
 # add cargo bin to PATH
 set -gx PATH $HOME/.cargo/bin $PATH
 
+# add nvim to PATH
+set -gx PATH /opt/nvim-linux-x86_64/bin $PATH
+
 # Created by `pipx` on 2025-05-25 01:59:15
 set PATH $PATH /home/isomo/.local/bin
 
 # SSH tunnel startup - check if tunnel is already running before starting
-if not pgrep -f "ssh -N -L 0.0.0.0:1080:127.0.0.1:1080 ubuntu@43.139.157.97" > /dev/null
-    nohup ssh -N -L 0.0.0.0:1080:127.0.0.1:1080 ubuntu@43.139.157.97 -i ~/.ssh/id_github >/dev/null 2>&1 &
-end
+# if not pgrep -f "ssh -N -L 0.0.0.0:1080:127.0.0.1:1080 ubuntu@43.139.157.97" > /dev/null
+#     nohup ssh -N -L 0.0.0.0:1080:127.0.0.1:1080 ubuntu@43.139.157.97 -i ~/.ssh/id_github >/dev/null 2>&1 &
+# end
